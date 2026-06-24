0587ab18b148053b7fd762a4c6a87459c8ee2872
vitaminE461 <2202_75861782@noreply.gitcode.com>
2026-06-24 20:46:48 +0800

    实现了扫描结果页跳转功能，包括其复制，分享文本，收藏功能

--- app/src/main/java/com/example/qrcode/activity/ScanItemDisplayActivity.kt
+++ app/src/main/java/com/example/qrcode/activity/ScanItemDisplayActivity.kt
@@ -1,21 +1,87 @@
 package com.example.qrcode.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.qrcode.R
+import com.example.qrcode.database.HistoryRecordDB
+import com.example.qrcode.databinding.ActivityScanItemDisplayBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class ScanItemDisplayActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_scan_item_display)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//扫描结果展示页面:负责展示二维码对应的文本信息 并提供 返回/复制/分享/收藏 功能
+class ScanItemDisplayActivity : BaseActivity<ActivityScanItemDisplayBinding>(
+    ActivityScanItemDisplayBinding::inflate) {
+    //定义变量
+    private var scanItemContent: String = ""
+    private var historyRecordId: Long = -1
+    private var isMarkedFavorite: Boolean = false
+    override fun initData() {
+        scanItemContent = intent.getStringExtra("EXTRA_SCAN_CONTENT").orEmpty()
+        historyRecordId = intent.getLongExtra("EXTRA_SCAN_ID",-1L)
+    }
+
+    override fun initView() {
+        binding.tvScanResultContent.text = scanItemContent
+        checkFavoritesStatus()  //检查收藏图标选中状态
+    }
+    //绑定监听事件
+    override fun initAction() {
+        binding.btnScanResultBack.setOnClickListener { finish() } //返回事件
+        binding.ivScanResultFavorites.setOnClickListener { toggleFavorite() } //切换收藏状态
+        binding.ivScanResultCopy.setOnClickListener {
+            val clipBoardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager //获取系统剪贴板服务
+            clipBoardManager.setPrimaryClip(ClipData.newPlainText("Scan",scanItemContent))  //设置刚复制的内容在剪贴板置顶
+            Toast.makeText(this,getString(R.string.scan_result_copy_hint), Toast.LENGTH_SHORT).show()
+        }
+        binding.ivScanResultShare.setOnClickListener {  //调用系统分享功能分享文本信息
+           val shareScanResultIntent  = Intent(Intent.ACTION_SEND).apply {
+               type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT,scanItemContent)
+            }
+            startActivity(Intent.createChooser(shareScanResultIntent,"Share_Result"))
         }
     }
+    //查询数据库获取当前记录的收藏状态
+    private fun checkFavoritesStatus() {
+       if (historyRecordId == -1L) return
+        lifecycleScope.launch(Dispatchers.IO) {
+                val dao = HistoryRecordDB.getDatabase(application).historyRecordDao()
+                val allHistoryRecords = dao.selectAllHistoryRecord()
+                val result = allHistoryRecords.find { it.id == historyRecordId }
+                //切回主线程更新UI图标
+                withContext(Dispatchers.Main){
+                    isMarkedFavorite = result?.isFavorite ?: false
+                    updateFavoritesIcon()
+                }
+        }
+    }
+    //切换收藏状态
+    fun toggleFavorite(){
+        if (historyRecordId == -1L) return
+        lifecycleScope.launch(Dispatchers.IO) {
+           val dao = HistoryRecordDB.getDatabase(application).historyRecordDao()
+            val newStatus = !isMarkedFavorite //状态取反
+            val newTime =  if (newStatus) System.currentTimeMillis() else null
+            //调用dao层接口直接更新数据
+            dao.updateFavoriteHistoryRecord(historyRecordId,newStatus,newTime)
+            //切回主线程更新状态和图标
+            withContext(Dispatchers.Main){
+                isMarkedFavorite = newStatus
+                updateFavoritesIcon()
+            }
+        }
+    }
+    //根据收藏状态更新UI图标状态
+    fun updateFavoritesIcon() {
+        binding.ivScanResultFavorites.setImageResource(
+            if (isMarkedFavorite) R.mipmap.ic_favorites_selected else R.mipmap.ic_favorites_unselected
+        )
+    }
+
+
 }
---
+++ app/src/main/java/com/example/qrcode/functions/scanFunction/ScanViewModel.kt
@@ -0,0 +1,76 @@
+package com.example.qrcode.functions.scanFunction
+
+import android.app.Application
+import androidx.camera.core.AspectRatio
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.qrcode.database.HistoryRecord
+import com.example.qrcode.database.HistoryRecordDB
+import com.example.qrcode.functions.createFunction.CreateType
+import com.example.qrcode.utils.SingleScanEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+//ScanViewModel:主要负责扫描界面的逻辑处理,包括UI层的(闪光灯/缩放),数据跳转/入库
+class ScanViewModel(application: Application): AndroidViewModel(application) {
+    //获取数据库访问实例Dao
+    private val historyRecordDao = HistoryRecordDB.getDatabase(application).historyRecordDao()
+    //定义闪光灯，缩放比例，是否跳转到结果页，以及是否还继续扫描状态
+    private val _flashState = MutableLiveData(false)
+    val flashState: LiveData<Boolean>  =_flashState
+    private val  _zoomRaio = MutableLiveData(0f)
+    val zoomRatio: LiveData<Float> = _zoomRaio
+    private val _navigateToResult = SingleScanEvent.singleScanEvent<Pair<HistoryRecord, Long>>()
+    val navigateToResult: LiveData<Pair<HistoryRecord, Long>> = _navigateToResult
+
+    var isProcessing: Boolean ?= false //判断当前帧线程池是否还有待处理的资源
+
+    //切换闪光灯状态
+     fun toggleFlashState(){
+         _flashState.value = !(_flashState.value ?: false)
+     }
+    //更新缩放比例:范围限定在0-1倍
+    fun updateZoomState(ratio: Float){
+        _zoomRaio.value =ratio.coerceIn(0f,1f)
+    }
+    //处理扫描结果
+    fun processScanResult(rawText: String){
+        //如果是正在处理，则直接拦截
+        if (isProcessing == true) return
+        isProcessing = true
+        //关于异步处理数据
+        val qrType = judgeTypeByText(rawText)
+        val historyRecordItem = HistoryRecord(
+            title = "Scan Result:${rawText.take(15)}",
+            content = rawText,
+            category = qrType
+        )
+        //将扫描数据异步存入数据库，并获取这条记录的id
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                val historyRecordId =  historyRecordDao.insertHistoryRecord(historyRecordItem)
+                _navigateToResult.value = historyRecordItem to historyRecordId  //跳转到对应结果页
+            }
+        }
+
+
+    }
+    fun judgeTypeByText(text: String): CreateType{
+        return when{
+            text.startsWith("http://") -> CreateType.WebSite
+            text.startsWith("WIFI:") -> CreateType.Wifi
+            text.startsWith("TEL:") -> CreateType.Tel
+            text.startsWith("SMSTO:") -> CreateType.SMS
+            else -> CreateType.Text
+        }
+    }
+
+    //重置处理锁 ，允许进行下一次扫描
+    fun resetProcessing(){
+        isProcessing = false
+    }
+
+}
